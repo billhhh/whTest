d1b0934b3cd1669ec2ba9162ad4ddb183992135e
Bill Dufresne <371570273@qq.com>
2016-01-23 18:45:22 +0800

    1

--- test/59 类声明固定数组.cpp
+++ test/59 类声明固定数组.cpp
@@ -4,16 +4,13 @@ using namespace std;
 #define GRAGH_LENGTH 180
 #define GRAGH_WIDTH 128
 
-typedef int (Mytype[GRAGH_WIDTH])[GRAGH_LENGTH];
+typedef int (Mytype[GRAGH_LENGTH])[GRAGH_WIDTH];
 
 class Test
 {
 public:
 	Mytype a;
-	Mytype get() {
-		return a;
-	}
-
+		
 protected:
 
 
@@ -27,10 +24,11 @@ int main()
 	Test t;
 
 	cout<<t.a<<endl;
-	cout<<t.get()<<endl;
 	cout<<&t.a[0][1]<<endl;
 
-
-
+	cout<<t.a[0][1]<<endl;
+	t.a[0][1] = 2;
+	cout<<t.a[179][127]<<endl;
+	
 	return 0;
 }
---
+++ test/60 数组深复制浅复制.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int a[3]={1,2,3};
+	int b[3];
+
+	b;
+
+	return 0; 
+}

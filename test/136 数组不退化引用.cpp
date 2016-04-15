36b91d7c386b768e64324aa95c0e7c6ac3010997
Bill Dufresne <371570273@qq.com>
2016-04-15 15:22:42 +0800

    数组传函数 返回值

---
+++ test/136 数组不退化引用.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <stdio.h>
+#include <cstring>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <set>
+#include <cassert>
+#include <time.h>
+#include <queue>
+#include <map>
+#include <stack>
+#include <bitset>
+#include <string>
+#include <sstream>
+#define INF 0x3f3f3f3f
+
+#define PRINT(x) cout<<x<<endl;
+
+using namespace std;
+
+template <class Type>
+Type stringToNum(const string& str)
+{
+	istringstream iss(str);
+	Type num;
+	iss >> num;
+	return num;    
+}
+
+//======================================================
+
+void func(int (&b) [20][10]) {
+	PRINT(b[20][1000]);
+}
+
+void func(int *b) {
+
+}
+
+void func2(int a[5]) {
+	PRINT(sizeof(a));
+	PRINT(a[3]);
+	PRINT(a[10]);
+}
+
+// int&[20] func3() {
+// 
+// 	int arr[20];
+// 	return arr;
+// }
+
+int main()
+{
+	//freopen("input.txt","r",stdin);
+
+	int a[20][10];
+
+	a[2][1] = 5;
+
+	cout<<a[22][12]<<endl;
+
+	func(a);
+
+	int b[4];
+	b[3] = 666;
+	func2(b);
+
+	return 0;
+}

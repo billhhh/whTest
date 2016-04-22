a884d744417e4a12fa68eab5cf54ddb259324e8a
Bill Dufresne <371570273@qq.com>
2016-04-22 15:11:38 +0800

    内存泄露

--- test/138 内存泄露检测.cpp
+++ test/138 内存泄露检测.cpp
@@ -1,3 +1,12 @@
+/*
+*
+* function: 内存泄露检测工具
+*
+* Date:2016-04-04
+*
+*    Author: Bill Wang
+*/
+
 #ifndef _DETECT_MEM_LEAK_H_
 #define _DETECT_MEM_LEAK_H_
 
@@ -10,6 +19,7 @@ using namespace std;
 
 #define new  new(_CLIENT_BLOCK, __FILE__, __LINE__)
 
+//控制台输出
 //on_ff 为true 打印内存泄漏, 为false则不打印
 //程序进入时调用
 void detect_memory_leaks(bool on_off) {
@@ -25,6 +35,18 @@ void detect_memory_leaks(bool on_off) {
 	_CrtSetDbgFlag(flags);
 }
 
+//输出窗口
+//on_ff 为true 打印内存泄漏, 为false则不打印
+void detect_memory_leaks2(bool on_off) {
+
+	if (!on_off) {
+		_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); //程序进入时调用
+		//_CrtDumpMemoryLeaks(); //程序退出时调用
+	}
+}
+
+#endif
+
 void GetMemory(char *p, int num)
 {
 	p = (char*)malloc(sizeof(char) * num);
@@ -32,15 +54,13 @@ void GetMemory(char *p, int num)
 
 int main(int argc,char** argv)
 {
+	detect_memory_leaks(true);
 	//_CrtSetDbgFlag ( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
 	char *str = NULL;
 	GetMemory(str, 100);
 	cout<<"Memory leak test!"<<endl;
 	//_CrtDumpMemoryLeaks();
-	detect_memory_leaks(true);
 	system("pause");
 
 	return 0;
 }
-
-#endif

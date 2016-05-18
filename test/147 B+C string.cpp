18346fdef304bcd5d8c5d039ed23e1591645f65e
Bill Dufresne <371570273@qq.com>
2016-05-19 07:47:37 +0800

    147

---
+++ test/147 B+C string.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stdio.h>
+#include <cstring>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <set>
+#include <cassert>
+#include <time.h>
+#include <queue>
+#include <map>
+#include <stack>
+#include <bitset>
+#include <string>
+#include <sstream>
+#define INF 0x3f3f3f3f
+
+#define PRINT(x) cout<<x<<endl;
+
+using namespace std;
+
+template <class Type>
+Type stringToNum(const string& str)
+{
+	istringstream iss(str);
+	Type num;
+	iss >> num;
+	return num;    
+}
+
+//======================================================
+
+int main()
+{
+	//freopen("input.txt","r",stdin);
+
+	string tmp = "B+C";
+	PRINT(tmp.length());
+
+	return 0;
+}
